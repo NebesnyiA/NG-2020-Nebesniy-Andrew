56027f79781594f2143eb96e092dba64abf6b69e
unknown <artpro197@gmail.com>
2021-01-25 20:30:33 +0300

    Reworked Lesson 3 Task 3

--- Lesson_3/Task_3/main.cpp
+++ Lesson_3/Task_3/main.cpp
@@ -5,19 +5,25 @@ using namespace std;
 int main()
 {
     char Str[100];
+    char Symbol;
 
     cout << "Enter your string, and see some black magic: " << endl;
     cin.getline(Str, 100);
+
+    cout << "BOOM! Now we have: " << endl;
 
     for(int i = 0; Str[i] != '\0'; i++)
     {
         if(Str[i] >= 'a' && Str[i] <= 'z')
         {
-            Str[i] = Str[i] - 32;
+            Symbol = Str[i] - 32;
+            cout << Symbol;
+        }
+        else
+        {
+            Symbol = Str[i];
+            cout << Symbol;
         }
     }
-
-    cout << "BOOM! Now we have: " << endl << Str;
-
     return 0;
 }
